69a379c8945a955e1d5dbe9c095cac1468d46dc4
DOZOU <akadama13@hotmail.co.jp>
2018-01-18 02:55:28 +0900

    setupを追加とそれに伴いフォルダを整理

--- Devices/IODevice.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-
-from pybration.DataSturucture import *
-
-
-class IODevice:
-    def __init__(self):
-        pass
---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name="pybration",
+    packages=['pybration'],
+    version="0.0.0",
+    author="takemuralabs",
+    author_email="",
+    url=""
+)

b047bda9ad0d8f9ad0e1e2d9569ddd4711d0b74d
DOZOU <akadama13@hotmail.co.jp>
2018-01-18 03:35:21 +0900

    フォルダ整理 その4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 requires = {"yapsy",
-            "pyqt5",
+            "PyQt5",
             "pyqtchart",
             "pandas",
             "matplotlib",
